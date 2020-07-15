3db97bf7e70553a9f796a3aeb30cf1a263f0ed67
UTG011 <68297862+UTG011@users.noreply.github.com>
2020-07-15 17:49:33 +0530

    Created positivenumbers.py

---
+++ positivenumbers.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+l1=[]
+l2=[]
+for i in list1:
+    if i>0:
+        l1.append(i)
+print("positive numbers in list1 are:",l1)
+for j in list2:
+    if j>0:
+        l2.append(j)
+print("positive numbers in list2 are:",l2)
